9642304dca50ea5f8a0e1bad02ba0f0f8cbff5bf
Massimo Noro <mnoro@yahoo.com>
2018-05-17 08:37:43 +0100

    Committing changes to Plot2

--- Plot2.R
+++ Plot2.R
@@ -2,8 +2,10 @@
 source("gettingData.R")
 
 ## 00. Plot2
-# Calculate new fields for timeseries
-x <- sort(result$date_time)
+
 with(result, plot(date_time, Global_active_power, type = "l", col = "black",
                   main = "Global Active Power", ylab = "Global Active Power (kilowatts)",
                   xlab=""))
+
+dev.copy(png, file = file.path(getwd(),"figure/Plot2.png"))
+dev.off()
